60af5b91cbe6a2703b16f5ab75c2d2c67a335506
Vinod Murukesan <vinodmurukesan@gmail.com>
2022-09-20 22:15:28 +0100

    Animation added

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -21,25 +21,54 @@ export default {
 
 <style scoped lang="scss">
 .header {
-  padding: 20px;
+  padding: 20px 20px 20px 60px;
   display: flex;
   background-color: #ccc;
+  position: relative;
 
   &__home-link {
-    display: flex;
     color: #333;
     text-decoration: none;
   }
 
   &__font-icon {
+    position: absolute;
+    left: 20px;
+    top: 20px;
     width: 30px;
     height: 30px;
-    margin: 0 10px 0 0;
   }
 
   &__title {
     margin: 5px 0 0;
     font-weight: bold;
+    position: relative;
+
+    &::before {
+      content: ' ';
+      position: absolute;
+      left: 50%;
+      bottom: 0px;
+      height: 2px;
+      width: 0%;
+      background-color: #333;
+      transition: all 0.8s ease;
+    }
+
+    &:hover {
+      &::before  {
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
+
+  &:hover &__font-icon {
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+    100% {transform: rotate(360deg);}
   }
 }
 </style>
